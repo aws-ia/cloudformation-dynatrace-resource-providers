69938958bbe836eb533a1730e42b8fc3d49accb6
Thomas Bouron <thomas.bouron@cloudsoftcorp.com>
2022-10-20 15:19:02 +0200

    Fix contract tests for `Dynatrace::Environment::SyntheticMonitor`

--- Dynatrace-Common/src/abstract-base-resource.ts
+++ Dynatrace-Common/src/abstract-base-resource.ts
@@ -19,7 +19,7 @@ export interface RetryableCallbackContext {
 
 export abstract class AbstractBaseResource<ResourceModelType extends BaseModel, GetResponseData, CreateResponseData, UpdateResponseData, ErrorType, TypeConfigurationType> extends BaseResource<ResourceModelType> {
 
-    private maxRetries = 5;
+    protected maxRetries = 5;
 
     /**
      * This method is invoked to get a resource from a vendor API, corresponding to the given the CloudFormation model input.
@@ -319,7 +319,7 @@ export abstract class AbstractBaseResource<ResourceModelType extends BaseModel, 
         }
     }
 
-    private async assertExists(model: ResourceModelType, typeConfiguration: TypeConfigurationType) {
+    protected async assertExists(model: ResourceModelType, typeConfiguration: TypeConfigurationType) {
         try {
             await this.get(model, typeConfiguration);
         } catch (e) {
--- Dynatrace-Environment-SyntheticMonitor/src/handlers.ts
+++ Dynatrace-Environment-SyntheticMonitor/src/handlers.ts
@@ -1,53 +1,146 @@
 import {ResourceModel, TypeConfigurationModel} from './models';
+import {RetryableCallbackContext} from '../../Dynatrace-Common/src/abstract-base-resource';
 import {AbstractDynatraceResource} from '../../Dynatrace-Common/src/abstract-dynatrace-resource';
 import {DynatraceClient} from '../../Dynatrace-Common/src/dynatrace-client';
 import {CaseTransformer, Transformer} from '../../Dynatrace-Common/src/util';
 import {version} from "../package.json";
+import {AxiosResponse} from "axios";
+import {
+    Action,
+    AwsTaskWorkerPool,
+    BaseModel,
+    Constructor,
+    exceptions,
+    handlerEvent,
+    HandlerSignatures,
+    LoggerProxy,
+    OperationStatus,
+    Optional,
+    ProgressEvent,
+    ResourceHandlerRequest,
+    SessionProxy
+} from "@amazon-web-services-cloudformation/cloudformation-cli-typescript-lib";
 
-type SyntheticMonitorPayload = {};
+type SyntheticMonitorPayload = {
+    entityId: string
+    script?: { [key: string]: any }
+};
 
 type SyntheticMonitorsPayload = {
     monitors: SyntheticMonitorPayload[]
 }
 
-class Resource extends AbstractDynatraceResource<ResourceModel, SyntheticMonitorPayload, SyntheticMonitorPayload, SyntheticMonitorPayload, TypeConfigurationModel> {
+type EventuallyConsistentCallbackContext = {
+    serverTiming?: number
+} & RetryableCallbackContext;
+
+class Resource extends AbstractDynatraceResource<ResourceModel, AxiosResponse<SyntheticMonitorPayload>, AxiosResponse<SyntheticMonitorPayload>, AxiosResponse<SyntheticMonitorPayload>, TypeConfigurationModel> {
 
     private userAgent = `AWS CloudFormation (+https://aws.amazon.com/cloudformation/) CloudFormation resource ${this.typeName}/${version}`;
+    private callbackDelay = 30;
 
-    async get(model: ResourceModel, typeConfiguration?: TypeConfigurationModel): Promise<SyntheticMonitorPayload> {
-        const response = await new DynatraceClient(typeConfiguration?.dynatraceAccess.endpoint, typeConfiguration?.dynatraceAccess.token, this.userAgent).doRequest<SyntheticMonitorPayload>(
+    constructor(typeName: string, modelTypeReference: Constructor<ResourceModel>, workerPool: AwsTaskWorkerPool, handlers: HandlerSignatures<ResourceModel, BaseModel>, typeConfigurationTypeReference: Constructor<BaseModel> & { deserialize: Function }) {
+        super(typeName, modelTypeReference, workerPool, handlers, typeConfigurationTypeReference);
+        this.maxRetries = 10;
+    }
+
+    @handlerEvent(Action.Update)
+    async updateHandler(
+        session: Optional<SessionProxy>,
+        request: ResourceHandlerRequest<ResourceModel>,
+        callbackContext: EventuallyConsistentCallbackContext,
+        logger: LoggerProxy,
+        typeConfiguration: TypeConfigurationModel
+    ): Promise<ProgressEvent<ResourceModel, EventuallyConsistentCallbackContext>> {
+        let model = this.newModel(request.desiredResourceState);
+
+        if (!(await this.assertExists(model, typeConfiguration))) {
+            throw new exceptions.NotFound(this.typeName, request.logicalResourceIdentifier);
+        }
+
+        if (callbackContext.retry && callbackContext.retry > this.maxRetries) {
+            throw new exceptions.NotStabilized(`Resource failed to stabilized after ${this.maxRetries} retries`);
+        }
+
+        if (!callbackContext.serverTiming) {
+            try {
+                await this.update(model, typeConfiguration);
+                let getResponse = await this.get(model, typeConfiguration);
+                let serverTiming = this.getServerTiming(getResponse);
+
+                let progressEventBuilder = ProgressEvent.builder<ProgressEvent<ResourceModel, EventuallyConsistentCallbackContext>>()
+                    .status(OperationStatus.InProgress)
+                    .resourceModel(model)
+                    .callbackContext({
+                        serverTiming: serverTiming,
+                        retry: 1
+                    });
+                if (!serverTiming) {
+                    progressEventBuilder.callbackDelaySeconds(this.callbackDelay);
+                }
+                return progressEventBuilder.build();
+            } catch (e) {
+                logger.log(`Error ${e}`);
+                this.processRequestException(e, request);
+            }
+        }
+
+        try {
+            const getResponse = await this.get(model, typeConfiguration);
+            const getServerTiming = this.getServerTiming(getResponse);
+
+            if (callbackContext.serverTiming && getServerTiming < callbackContext.serverTiming) {
+                return ProgressEvent.builder<ProgressEvent<ResourceModel, EventuallyConsistentCallbackContext>>()
+                    .status(OperationStatus.InProgress)
+                    .resourceModel(model)
+                    .callbackContext({
+                        serverTiming: getServerTiming,
+                        retry: callbackContext.retry + 1
+                    }).build();
+            }
+
+            model = this.setModelFrom(model, getResponse);
+            return ProgressEvent.success<ProgressEvent<ResourceModel, EventuallyConsistentCallbackContext>>(model);
+        } catch (e) {
+            if (!callbackContext.serverTiming) {
+                throw new exceptions.NotStabilized(`Resource failed to stabilized after ${this.callbackDelay} seconds: ${e.message}`);
+            } else {
+                this.processRequestException(e, request);
+            }
+        }
+    }
+
+    async get(model: ResourceModel, typeConfiguration?: TypeConfigurationModel): Promise<AxiosResponse<SyntheticMonitorPayload>> {
+        return await new DynatraceClient(typeConfiguration?.dynatraceAccess.endpoint, typeConfiguration?.dynatraceAccess.token, this.userAgent).doRequest<SyntheticMonitorPayload>(
             'get',
             `/api/v1/synthetic/monitors/${model.entityId}`);
-        return response.data;
     }
 
     async list(model: ResourceModel, typeConfiguration?: TypeConfigurationModel): Promise<ResourceModel[]> {
         const response = await new DynatraceClient(typeConfiguration?.dynatraceAccess.endpoint, typeConfiguration?.dynatraceAccess.token, this.userAgent).doRequest<SyntheticMonitorsPayload>(
             'get',
             `/api/v1/synthetic/monitors`);
-        return response.data.monitors.map(monitorPayload => this.setModelFrom(model, monitorPayload))
+        return response.data.monitors.map(monitorPayload => this.setModelFrom(model, {data: monitorPayload} as AxiosResponse<SyntheticMonitorPayload>))
     }
 
-    async create(model: ResourceModel, typeConfiguration?: TypeConfigurationModel): Promise<SyntheticMonitorPayload> {
-        const response = await new DynatraceClient(typeConfiguration?.dynatraceAccess.endpoint, typeConfiguration?.dynatraceAccess.token, this.userAgent).doRequest<SyntheticMonitorPayload>(
+    async create(model: ResourceModel, typeConfiguration?: TypeConfigurationModel): Promise<AxiosResponse<SyntheticMonitorPayload>> {
+        return await new DynatraceClient(typeConfiguration?.dynatraceAccess.endpoint, typeConfiguration?.dynatraceAccess.token, this.userAgent).doRequest<SyntheticMonitorPayload>(
             'post',
             '/api/v1/synthetic/monitors',
             {},
             Transformer.for(model.toJSON())
                 .transformKeys(CaseTransformer.PASCAL_TO_CAMEL)
                 .transform());
-        return response.data;
     }
 
-    async update(model: ResourceModel, typeConfiguration?: TypeConfigurationModel): Promise<SyntheticMonitorPayload> {
-        let response = await new DynatraceClient(typeConfiguration?.dynatraceAccess.endpoint, typeConfiguration?.dynatraceAccess.token, this.userAgent).doRequest<SyntheticMonitorPayload>(
+    async update(model: ResourceModel, typeConfiguration?: TypeConfigurationModel): Promise<AxiosResponse<SyntheticMonitorPayload>> {
+        return await new DynatraceClient(typeConfiguration?.dynatraceAccess.endpoint, typeConfiguration?.dynatraceAccess.token, this.userAgent).doRequest<SyntheticMonitorPayload>(
             'put',
             `/api/v1/synthetic/monitors/${model.entityId}`,
             {},
             Transformer.for(model.toJSON())
                 .transformKeys(CaseTransformer.PASCAL_TO_CAMEL)
                 .transform());
-        return response.data;
     }
 
     async delete(model: ResourceModel, typeConfiguration?: TypeConfigurationModel): Promise<void> {
@@ -60,17 +153,34 @@ class Resource extends AbstractDynatraceResource<ResourceModel, SyntheticMonitor
         return new ResourceModel(partial);
     }
 
-    setModelFrom(model: ResourceModel, from?: SyntheticMonitorPayload): ResourceModel {
-        if (!from) {
+    setModelFrom(model: ResourceModel, from?: AxiosResponse<SyntheticMonitorPayload>): ResourceModel {
+        if (!from.data) {
             return model;
         }
 
         const resourceModel = new ResourceModel({
             ...model,
-            ...from
+            ...Transformer.for(from.data)
+                .transformKeys(CaseTransformer.IDENTITY)
+                .forModelIngestion()
+                .transform()
         });
         delete resourceModel.type_;
+        delete resourceModel.script;
+        delete resourceModel.tags;
+
         return resourceModel;
+    }
+
+    private getServerTiming(response: AxiosResponse<any>) {
+        let serverTiming = undefined;
+        if (response.headers.hasOwnProperty('server-timing')) {
+            const matches = response.headers['server-timing'].match(/dtRpid;desc="([\d\-]+)"/);
+            if (matches !== null) {
+                serverTiming = parseInt(matches[1]);
+            }
+        }
+        return serverTiming;
     }
 
 }
