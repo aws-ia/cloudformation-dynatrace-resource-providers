8310d7cac8a0b6f0846ce5d3a7f391906effe94c
Rajesh Duraisamy <rajdnp@amazon.com>
2023-02-27 15:18:06 -0800

    Adding Dynatrace to CICD

--- Dynatrace-Environment-SyntheticLocation/setup.sh
+++ Dynatrace-Environment-SyntheticLocation/setup.sh
@@ -7,5 +7,3 @@ cat example_inputs/inputs_1_create.json > inputs/inputs_1_create.json
 cat example_inputs/inputs_1_invalid.json > inputs/inputs_1_invalid.json
 cat example_inputs/inputs_1_update.json > inputs/inputs_1_update.json
 cat test/integ-template.yml  > test/integ.yml
-cat inputs/inputs_1_update.json
-cat inputs/inputs_1_create.json
